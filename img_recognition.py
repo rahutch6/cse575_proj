760ec8abf2e2b2b78927dfb58ad38452303c58ca
Renee Hutchins <rahutch6@asu.edu>
2025-04-11 14:33:22 -0700

    cleanup images

--- img_recognition.py
+++ img_recognition.py
@@ -36,3 +36,65 @@ val_ds = tf.keras.utils.image_dataset_from_directory(
     batch_size=32)
 class_names = train_ds.class_names 
 
+plt.figure(figsize=(10, 10)) 
+
+for images, labels in train_ds.take(1): 
+	for i in range(25): 
+		ax = plt.subplot(5, 5, i + 1) 
+		plt.imshow(images[i].numpy().astype("uint8")) 
+		plt.title(class_names[labels[i]]) 
+		plt.axis("off") 
+
+
+num_classes = len(class_names) 
+
+model = Sequential([ 
+	layers.Rescaling(1./255, input_shape=(256,256, 3)), 
+	layers.Conv2D(16, 3, padding='same', activation='relu'), 
+	layers.MaxPooling2D(), 
+	layers.Conv2D(32, 3, padding='same', activation='relu'), 
+	layers.MaxPooling2D(), 
+	layers.Conv2D(64, 3, padding='same', activation='relu'), 
+	layers.MaxPooling2D(), 
+	layers.Flatten(), 
+	layers.Dense(128, activation='relu'), 
+	layers.Dense(num_classes) 
+]) 
+
+model.compile(optimizer='adam', 
+			loss=tf.keras.losses.SparseCategoricalCrossentropy( 
+				from_logits=True), 
+			metrics=['accuracy']) 
+model.summary() 
+
+epochs=2
+history = model.fit( 
+    train_ds, 
+    validation_data=val_ds, 
+    epochs=epochs 
+    ) 
+#Accuracy 
+acc = history.history['accuracy'] 
+val_acc = history.history['val_accuracy'] 
+
+#loss 
+loss = history.history['loss'] 
+val_loss = history.history['val_loss'] 
+
+#epochs 
+epochs_range = range(epochs) 
+
+#Plotting graphs 
+plt.figure(figsize=(8, 8)) 
+plt.subplot(1, 2, 1) 
+plt.plot(epochs_range, acc, label='Training Accuracy') 
+plt.plot(epochs_range, val_acc, label='Validation Accuracy') 
+plt.legend(loc='lower right') 
+plt.title('Training and Validation Accuracy') 
+
+plt.subplot(1, 2, 2) 
+plt.plot(epochs_range, loss, label='Training Loss') 
+plt.plot(epochs_range, val_loss, label='Validation Loss') 
+plt.legend(loc='upper right') 
+plt.title('Training and Validation Loss') 
+plt.show() 
